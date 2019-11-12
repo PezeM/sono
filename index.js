14536bf6cbaca93ba1508943a1202c9cd2a27b3e
PezeM <16854655+PezeM@users.noreply.github.com>
2019-11-13 00:30:33 +0100

    More files

---
+++ index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+exports.__esModule = true;
+
+var _sono = require('./core/sono');
+
+var _sono2 = _interopRequireDefault(_sono);
+
+require('./effects');
+
+require('./utils');
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = _sono2.default;
